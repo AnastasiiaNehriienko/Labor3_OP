c8cfa12ea4c027b0479d69480fd69da1fde35166
AnastasiiaNehriienko <89258252+AnastasiiaNehriienko@users.noreply.github.com>
2021-10-04 15:53:38 +0300

    Add files via upload

---
+++ Labor3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+// із заданою точністю визначити значення математичної константи e=1+1/1!+1/2!+1/3!...
+
+int main() {
+	double n; // точність 
+	int k = 1;
+	double add = 1, add_0;
+	double e = 2;
+	std::cout << "Enter the accuracy (number of terms): ";
+	std::cin >> n;
+	do {
+		k++;
+		add_0 = add;
+		add = add_0 / k;
+		e += add;
+	} while ((add_0 - add) >= n);
+		std::cout << "e=" << e;
+	return 0;
+}
